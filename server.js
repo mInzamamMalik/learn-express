73695af60674c1d69d5ace419aa1cc5f5eaac8d8
Inzamam Malik <malikasinger1@gmail.com>
2017-02-13 18:39:06 +0500

    mongoose connected

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 var express = require("express");
-
+var mongoose = require("mongoose");
 
 var app = express();
-
 var port = (process.env.PORT || 3000);
 
 
 app.get("/", function (req, res, next) {
-
     console.log("reauest is comming to '/' ");
-
     res.send("Hello world");
-
 });
-
-
-
 
 app.listen(port, function () {
     console.log('app is running on port', port);
+});
+
+mongoose.connect("mongodb://malikasinger:pakistan1@ds149049.mlab.com:49049/malikasinger-demo");
+
+mongoose.connection.on('connected', function () {
+    console.log("Mongoose is connected");
 });
